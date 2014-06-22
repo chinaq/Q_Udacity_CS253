63a5947da8231cfd3617b8b3f6091a5ecd75906b
chinaq <aaa6263@sina.com>
2014-06-22 21:35:15 +0800

    Lesson3_AsciiChan to Start

--- Lesson3/asciichan/main.py
+++ Lesson3/asciichan/main.py
@@ -15,10 +15,32 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
+from google.appengine.ext import db
 
-class MainHandler(webapp2.RequestHandler):
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), 
+	autoescape=True)
+
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(**params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+
+
+class MainHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render("front.html")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
